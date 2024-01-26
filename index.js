edaeb72d6e358105c8ecc403a686fde8649c823e
hopelesssoulx <hopelesssoulx@gmail.com>
2024-01-26 16:36:14 +0800

    abaaba

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 16168
+const express = require("express");
+const app = express();
+const port = 16168;
 
+const cors = require("cors");
+app.use(cors());
 
-const cors = require('cors')
-app.use(cors())
+const path = require("path");
+const fs = require("fs");
 
+const db = require("better-sqlite3")(
+  "D:\\D\\work\\learning_sqlite\\learning.db"
+);
 
-const db = require('better-sqlite3')('D:\\D\\work\\learning_sqlite\\learning.db');
-const sections = db.prepare('SELECT distinct section FROM fullvoice').all();
-let obj = {}
-sections.forEach(item => {
-    obj[item.section] = []
-})
-const files = db.prepare('SELECT * FROM fullvoice').all();
-files.forEach(item => {
-    obj[item.section].push(item.file_name)
-})
-
-
-const fs = require('fs')
-fs.writeFile('test.json', JSON.stringify(obj, null, 2), err => {
-    if (err) {
-        console.error(err);
-    }
+let obj = {};
+const sections = db.prepare("SELECT distinct section FROM fullvoice").all();
+sections.forEach((item) => {
+  obj[item.section] = [];
+});
+const files = db.prepare("SELECT * FROM fullvoice").all();
+files.forEach((item) => {
+  obj[item.section].push({
+    name: item.file_name,
+    character: item.character,
+    ori: item.ori,
+    chs: item.chs,
+    eng: item.eng,
+    otherLanguage: item.other_language,
+  });
 });
 
+fs.writeFile("test.json", JSON.stringify(obj, null, 2), (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
 
-var path = require("path");
-app.get('/getFile/*', (req, res) => {
-    return res.sendFile(path.join('D:\\D\\download\\z_test\\', `${req.params[0]}`))
-    // return res.download(path.join('D:\\D\\download\\z_test\\', `${req.params[0]}`))
-    // return res.send('Hello World!')
-})
-app.get('/getList', (req, res) => {
-    let list = require('./test.json')
+app.get("/getFile/*", (req, res) => {
+  return res.sendFile(
+    path.join("D:\\D\\download\\z_test\\", `${req.params[0]}`)
+  );
+  // return res.download(path.join('D:\\D\\download\\z_test\\', `${req.params[0]}`))
+  // return res.send('Hello World!')
+});
+app.get("/getList", (req, res) => {
+  let list = require("./test.json");
 
-    res.header("Content-Type", 'application/json');
-    return res.send(JSON.stringify(list))
-})
-
+  res.header("Content-Type", "application/json");
+  return res.send(list);
+});
 
 app.listen(port, () => {
-    console.log(`${port}=======================================`)
-})
+  console.log(`${port}=======================================`);
+});
