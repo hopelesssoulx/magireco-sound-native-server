541becdff6c1fbb0e561ec77c716ec04ec2591c1
hopelesssoulx <hopelesssoulx@gmail.com>
2024-02-23 16:40:17 +0800

    abaaba

--- index.js
+++ index.js
@@ -13,66 +13,156 @@ const db = require("better-sqlite3")(
 );
 
 const resourcePath =
-  "D:\\D\\work\\etc\\N\\magireco-data-downloader\\py3\\resource\\sound_native\\fullvoice\\";
-// const resourcePath = "D:\\D\\download\\z_test\\";
+  "D:\\D\\work\\etc\\N\\magireco-data-downloader\\py3\\resource\\sound_native\\";
 
 /**
  * read dir
  */
-let fullvoiceData = [];
+let bgm = [];
+let fullvoice = [];
+let jingle = [];
+let surround = [];
+let voice = [];
 function traverseDir(dir) {
   fs.readdirSync(dir).forEach((file) => {
     let fullPath = path.join(dir, file);
     if (fs.lstatSync(fullPath).isDirectory()) {
       traverseDir(fullPath);
     } else {
-      fullvoiceData.push({
-        section: fullPath.split("\\").at(-2),
-        file_name: fullPath.split("\\").at(-1),
-      });
+      if (fullPath.split("\\").at(-3) == "fullvoice") {
+        fullvoice.push({
+          section: fullPath.split("\\").at(-2),
+          file_name: fullPath.split("\\").at(-1),
+        });
+      }
+      if (fullPath.split("\\").at(-2) == "bgm") {
+        let fileName = fullPath.split("\\").at(-1);
+        if (fileName.split(".").at(-1) == "hca") {
+          let group = fileName.split("_")[0];
+          bgm.push({
+            group: group,
+            file_name: fileName,
+          });
+        }
+      }
+      if (fullPath.split("\\").at(-2) == "voice") {
+        let fileName = fullPath.split("\\").at(-1);
+        let character = fileName.split("_").at(1) + fileName.split("_").at(2);
+        voice.push({
+          character: character,
+          file_name: fileName,
+        });
+      }
     }
   });
 }
-traverseDir(resourcePath);
+// traverseDir(resourcePath);
 
 /**
  * insert db
  */
-const insertFolderSQL = db.prepare(
+const insertBgmSQL = db.prepare(
+  "INSERT INTO bgm ('group', file_name) VALUES (@group, @file_name)"
+);
+const insertBgm = db.transaction((items) => {
+  for (let item of items) insertBgmSQL.run(item);
+});
+
+const insertFullvoiceSQL = db.prepare(
   "INSERT INTO fullvoice (section, file_name) VALUES (@section, @file_name)"
 );
-const insertFolder = db.transaction((items) => {
-  for (let item of items) insertFolderSQL.run(item);
+const insertFullvoice = db.transaction((items) => {
+  for (let item of items) insertFullvoiceSQL.run(item);
 });
+
+const insertVoiceSQL = db.prepare(
+  "INSERT INTO voice (character, file_name) VALUES (@character, @file_name)"
+);
+const insertVoice = db.transaction((items) => {
+  for (let item of items) insertVoiceSQL.run(item);
+});
+
 try {
-  insertFolder(fullvoiceData);
-} catch (err) {}
+  // insertBgm(bgm);
+  // insertFullvoice(fullvoice);
+  // insertVoice(voice);
+} catch (err) {
+  console.log(err);
+}
 
 /**
  * read db
  */
-let obj = {};
-const sections = db.prepare("SELECT distinct section FROM fullvoice").all();
-sections.forEach((item) => {
-  obj[item.section] = [];
-});
-const files = db.prepare("SELECT * FROM fullvoice").all();
-files.forEach((item) => {
-  obj[item.section].push({
-    name: item.file_name,
-    character: item.character,
-    ori: item.ori,
-    chs: item.chs,
-    eng: item.eng,
-    otherLanguage: item.other_language,
-    remark: item.remark,
+function getBgmObj() {
+  let bgmObj = {};
+  const group = db.prepare("SELECT distinct [group] FROM bgm").all();
+  group.forEach((item) => {
+    bgmObj[item.group] = [];
   });
-});
+  const bgmFiles = db.prepare("SELECT * FROM bgm").all();
+  bgmFiles.forEach((item) => {
+    bgmObj[item.group].push({
+      group: item.group,
+      name: item.file_name,
+      remark: item.remark,
+    });
+  });
+  return bgmObj;
+}
+
+function getFullvoiceObj() {
+  let fullvoiceObj = {};
+  const sections = db.prepare("SELECT distinct section FROM fullvoice").all();
+  sections.forEach((item) => {
+    fullvoiceObj[item.section] = [];
+  });
+  const fullvoiceFiles = db.prepare("SELECT * FROM fullvoice").all();
+  fullvoiceFiles.forEach((item) => {
+    fullvoiceObj[item.section].push({
+      name: item.file_name,
+      character: item.character,
+      ori: item.ori,
+      chs: item.chs,
+      eng: item.eng,
+      otherLanguage: item.other_language,
+      remark: item.remark,
+    });
+  });
+  return fullvoiceObj;
+}
+
+function getVoiceObj() {
+  let voiceObj = {};
+  const character = db.prepare("SELECT distinct character FROM voice").all();
+  character.forEach((item) => {
+    voiceObj[item.character] = [];
+  });
+  const voiceFiles = db.prepare("SELECT * FROM voice").all();
+  voiceFiles.forEach((item) => {
+    voiceObj[item.character].push({
+      character: item.character,
+      name: item.file_name,
+      ori: item.ori,
+      chs: item.chs,
+      eng: item.eng,
+      otherLanguage: item.other_language,
+      remark: item.remark,
+    });
+  });
+  return voiceObj;
+}
 
 /**
  * write json
  */
-fs.writeFile("test.json", JSON.stringify(obj), (err) => {
+let soundNative = {
+  bgm: getBgmObj(),
+  fullvoice: getFullvoiceObj(),
+  jingle: null,
+  surround: null,
+  voice: getVoiceObj(),
+};
+fs.writeFile("sound-native.json", JSON.stringify(soundNative), (err) => {
   if (err) {
     console.error(err);
   }
@@ -85,18 +175,14 @@ app.get("/getFile/*", (req, res) => {
   return res.download(path.join(resourcePath, `${req.params[0]}`));
 });
 app.get("/getList", (req, res) => {
-  let list = require("./test.json");
+  let list = require("./sound-native.json");
 
   res.header("Content-Type", "application/json");
-  return res.send({
-    bgm: null,
-    fullvoice: list,
-    jingle: null,
-    surround: null,
-    voice: null,
-  });
+  return res.send(list);
 });
 
 app.listen(port, () => {
-  console.log(`${port}=======================================`);
+  console.log(
+    `${port}, ${new Date().toLocaleString()}=======================================`
+  );
 });
